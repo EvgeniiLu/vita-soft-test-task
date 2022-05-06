8f9ded1d8a53a4f1f8a39ccf15400211eff5e51c
Lushchai Evgenii <lushayevgen@mail.ru>
2022-05-06 16:44:07 +0300

    add layout and modalpostarea component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,152 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="app">
+    <div class="container">
+      <template v-if="posts.length">
+        <div
+          class="post"
+          @click="dialogVisible = true"
+          v-for="item of posts"
+          :key="item"
+        >
+          <el-card shadow="hover">
+            <div class="content">
+              <div class="post-title">lorem</div>
+              <div class="post-description">lorem</div>
+            </div>
+            <div class="post-comments-counter">
+              <el-badge :value="50" type="primary">
+                <div class="el-icon-chat-line-round" />
+              </el-badge>
+            </div>
+            <div class="edit">
+              <el-button type="primary" icon="el-icon-edit" circle></el-button>
+
+              <el-popconfirm
+                confirm-button-text="OK"
+                cancel-button-text="No, Thanks"
+                icon="el-icon-info"
+                title="Are you sure to delete this?"
+              >
+                <el-button
+                  slot="reference"
+                  type="danger"
+                  icon="el-icon-delete"
+                  circle
+                ></el-button>
+              </el-popconfirm>
+            </div>
+          </el-card>
+        </div>
+      </template>
+
+      <template v-else>
+        <h1>Нет постов</h1>
+      </template>
+
+      <div class="modal-post">
+        <el-dialog title="Tips" :visible.sync="dialogVisible" width="90%">
+          <div class="post-title">lorem</div>
+          <div class="post-description">lorem</div>
+          <div class="post-comments">lorem</div>
+
+          <div class="form">
+            <el-form
+              :model="ruleForm"
+              :rules="rules"
+              ref="ruleForm"
+              label-width="120px"
+              class="demo-ruleForm"
+            >
+              <el-form-item label="Activity name" prop="name">
+                <el-input v-model="ruleForm.name"></el-input>
+              </el-form-item>
+              <el-form-item label="Activity form" prop="desc">
+                <el-input type="textarea" v-model="ruleForm.desc"></el-input>
+              </el-form-item>
+              <el-form-item>
+                <el-button type="primary" @click="submitForm('ruleForm')"
+                  >Create</el-button
+                >
+              </el-form-item>
+            </el-form>
+          </div>
+        </el-dialog>
+      </div>
+      <modal-post-area />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import ModalPostArea from "./components/ModalPostArea.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    ModalPostArea,
+  },
+
+  data() {
+    return {
+      posts: [],
+
+      dialogVisible: false,
+
+      ruleForm: {
+        name: "",
+        desc: "",
+      },
+      rules: {
+        name: [
+          {
+            required: true,
+            message: "Please input Activity name",
+            trigger: "blur",
+          },
+          {
+            min: 3,
+            max: 5,
+            message: "Length should be 3 to 5",
+            trigger: "blur",
+          },
+        ],
+        desc: [
+          {
+            required: true,
+            message: "Please input activity form",
+            trigger: "blur",
+          },
+        ],
+      },
+    };
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          alert("submit!");
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
+    },
   },
 };
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss" scoped>
+.container {
+  width: 100%;
+  max-width: 1300px;
+  margin: 0 auto;
+}
+
+.post-comments-counter {
+  font-size: 32px;
+}
+
+.el-badge__content.is-fixed {
+  border: none;
 }
 </style>
---
+++ src/components/ModalPostArea.vue
@@ -0,0 +1,111 @@
+<template>
+  <div class="modal-add-post">
+    <div class="add-post" @click="createPost">
+      <el-tooltip effect="light" content="Add Post" placement="left">
+        <div class="el-icon-circle-plus" />
+      </el-tooltip>
+    </div>
+    <el-dialog :title="titlePost" :visible.sync="dialogVisible" width="90%">
+      <div class="form">
+        <el-form :model="form" :rules="rules" ref="ruleForm">
+          <el-form-item prop="title">
+            <el-input v-model="form.title" placeholder="Заголовок"></el-input>
+          </el-form-item>
+          <el-form-item prop="desc">
+            <el-input
+              v-model="form.desc"
+              placeholder="Краткое описание"
+            ></el-input>
+          </el-form-item>
+          <el-form-item prop="text">
+            <el-input
+              type="textarea"
+              v-model="form.text"
+              placeholder="Введите текст"
+            ></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="submitForm('ruleForm')"
+              >Добавить</el-button
+            >
+          </el-form-item>
+        </el-form>
+      </div>
+    </el-dialog>
+  </div>
+</template>
+<script>
+export default {
+  name: "AddPost",
+  data() {
+    return {
+      titlePost: "",
+
+      dialogVisible: false,
+      form: {
+        title: "",
+        desc: "",
+        text: "",
+      },
+      rules: {
+        title: [
+          {
+            required: true,
+            message: "Please input Activity name",
+            trigger: "blur",
+          },
+          {
+            min: 3,
+            max: 20,
+            message: "Length should be 3 to 5",
+            trigger: "blur",
+          },
+        ],
+        desc: [
+          {
+            required: true,
+            message: "Please input activity form",
+            trigger: "blur",
+          },
+        ],
+        text: [
+          {
+            required: true,
+            message: "Please input activity form",
+            trigger: "blur",
+          },
+        ],
+      },
+    };
+  },
+  methods: {
+    createPost() {
+      this.dialogVisible = true;
+      this.titlePost = "Создать пост";
+    },
+
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          this.$emit("addPost", this.form);
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+.add-post {
+  font-size: 60px;
+  position: fixed;
+  bottom: 50px;
+  right: 50px;
+  color: #409eff;
+}
+.add-post:hover {
+  opacity: 0.75;
+}
+</style>
